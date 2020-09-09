c4e4e1139330612a3980c67a5293172a5daba61e
KKasian <kkasian@gmail.com>
2020-09-09 14:13:38 +0300

    Initial commit

---
+++ main.py
@@ -0,0 +1,14 @@
+def  function (a, b,):
+   return a + b
+print (function (2, 10,))
+
+def  function (a, b, c):
+   return a + b > c
+print (function (2, 10, 20))
+
+def  function (a, b, c):
+   return a + b < c
+print (function (2, 10, 20))
+def  function (a, b, c):
+   return a + b <= c
+print (function (2, 10, 20))
